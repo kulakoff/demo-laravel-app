f8ad1b5bb286094629567b1f99c09043f8bb3f5e
Anton Kulakov <anton.kulakoff@ya.ru>
2022-12-14 23:50:54 +0300

    feat: незначительные правки

--- routes/web.php
+++ routes/web.php
@@ -26,11 +26,17 @@ Route::get('/contact', function () {
     return view('contact');
 })->name('contact');
 
-Route::post('/contact/submit', [App\Http\Controllers\ContactController::class, "submit"] )->name('contact-form');
+//Вывод всех сообщений
 Route::get('/contact/all', [App\Http\Controllers\ContactController::class, "allData"] )->name('contact-data');
+//Данные формы контактов
+Route::post('/contact/submit', [App\Http\Controllers\ContactController::class, "submit"] )->name('contact-form');
+//Страничка с выбранным сообщением
 Route::get('/contact/all/{message_id}', [App\Http\Controllers\ContactController::class, "showOneMessage"] )->name('contact-data-one');
+//Страница редактирования записи
 Route::get('/contact/all/{message_id}/update', [App\Http\Controllers\ContactController::class, "updateMessage"] )->name('contact-update');
+//Данные формы редактирования записи
 Route::post('/contact/all/{message_id}/update', [App\Http\Controllers\ContactController::class, "updateMessageSubmit"] )->name('contact-update-submit');
+//Удаления сообщения по ID
 Route::get('/contact/all/{message_id}/delete', [App\Http\Controllers\ContactController::class, "deleteMessage"] )->name('contact-delete');
 
 
