6337f1c29e7d0b6f4ede023244ec3d4a6bd45d1f
Anton Kulakov <anton.kulakoff@ya.ru>
2022-12-14 12:10:28 +0300

    feat: добавлена валидация на стороне front и back

--- resources/views/contact.blade.php
+++ resources/views/contact.blade.php
@@ -17,19 +17,19 @@
         @csrf
         <div class="form-group">
             <label for="name">Имя</label>
-            <input type="text" name="name" placeholder="Введите имя" id="name" class="form-control">
+            <input type="text" name="name" placeholder="Введите имя" id="name" required class="form-control">
         </div>
         <div class="form-group">
             <label for="email">Адрес электронной почты</label>
-            <input type="email" name="email" placeholder="Введите email" id="email" class="form-control">
+            <input type="email" name="email" placeholder="Введите email" id="email" required class="form-control">
         </div>
         <div class="form-group">
             <label for="subject">Тема сообщения</label>
-            <input type="text" name="subject" placeholder="Введите тему собщения" id="subject" class="form-control">
+            <input type="text" name="subject" placeholder="Введите тему собщения" id="subject" required class="form-control">
         </div>
         <div class="form-group">
             <label for="subject">Сообщение</label>
-            <textarea name="message" id="message" placeholder="Введите сообщение" class="form-control"></textarea>
+            <textarea name="message" id="message" placeholder="Введите сообщение" required class="form-control"></textarea>
         </div>
         <button type="submit" class="btn btn-success mt-2">Отправить</button>
     </form>
