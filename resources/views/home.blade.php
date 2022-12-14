47ae1512757dddc431c332324621a51bf8ba7543
Anton Kulakov <anton.kulakoff@ya.ru>
2022-12-15 00:00:14 +0300

    init: start project spa-crud-inertia

--- resources/views/home.blade.php
+++
@@ -1,9 +0,0 @@
-@extends('layouts.app')
-
-@section('title-block')Главная страница@endsection
-
-@section('content')
-    <h1>Главная страница</h1>
-    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Assumenda atque autem commodi dolores doloribus et eum
-        ex laudantium, minus molestias nostrum odio optio pariatur praesentium quam quisquam, reiciendis rem sequi.</p>
-@endsection
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,5 @@
 <?php
 
-//use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -15,32 +14,5 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('home');
-})->name('home');
-
-Route::get('/about', function () {
-    return view('about');
-})->name('about');
-
-Route::get('/contact', function () {
-    return view('contact');
-})->name('contact');
-
-//Вывод всех сообщений
-Route::get('/contact/all', [App\Http\Controllers\ContactController::class, "allData"] )->name('contact-data');
-//Данные формы контактов
-Route::post('/contact/submit', [App\Http\Controllers\ContactController::class, "submit"] )->name('contact-form');
-//Страничка с выбранным сообщением
-Route::get('/contact/all/{message_id}', [App\Http\Controllers\ContactController::class, "showOneMessage"] )->name('contact-data-one');
-//Страница редактирования записи
-Route::get('/contact/all/{message_id}/update', [App\Http\Controllers\ContactController::class, "updateMessage"] )->name('contact-update');
-//Данные формы редактирования записи
-Route::post('/contact/all/{message_id}/update', [App\Http\Controllers\ContactController::class, "updateMessageSubmit"] )->name('contact-update-submit');
-//Удаления сообщения по ID
-Route::get('/contact/all/{message_id}/delete', [App\Http\Controllers\ContactController::class, "deleteMessage"] )->name('contact-delete');
-
-
-
-//Auth::routes();
-//
-//Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+    return view('welcome');
+});
