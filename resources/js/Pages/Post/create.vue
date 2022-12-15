e1a59ba2d50ab5eae3f894c15fd8a5537a7c1679
Anton Kulakov <anton.kulakoff@ya.ru>
2022-12-15 13:37:21 +0300

    feat: добавлена стестовая страница с формой

--- resources/js/Pages/Post/create.vue
+++ resources/js/Pages/Post/create.vue
@@ -1,15 +1,20 @@
 <template>
     <div class="w-96 mx-auto pt-8">
         <h1 class="text-lg mb-8">Create</h1>
-        <form>
+        <form @submit.prevent="store">
             <div class="mb-4">
-                <input class="w-full rounded-full border-gray-300" type="text" placeholder="Title">
+                <input v-model="title" class="w-full rounded-full border-gray-300 align-middle" type="text"
+                       placeholder="Title">
             </div>
             <div class="mb-4">
-               <textarea class="w-full rounded-full border-gray-300" placeholder="Content"></textarea>
+                <textarea v-model="content" class="w-full rounded-full border-gray-300"
+                          placeholder="Content"></textarea>
             </div>
             <div>
-                <button class="ml-auto hover:bg-white hover:text-sky-500 block p-2 w-32 border border-sky-500 bg-sky-500 rounded-full text-center text-white" type="submit">Create</button>
+                <button
+                    class="ml-auto hover:bg-white hover:text-sky-500 block p-2 w-32 border border-sky-500 bg-sky-500 rounded-full text-center text-white"
+                    type="submit">Create
+                </button>
             </div>
         </form>
     </div>
@@ -17,10 +22,24 @@
 
 <script>
 import {Link} from "@inertiajs/inertia-vue3";
+
 export default {
     name: "index",
-    components:{
+    components: {
         Link
+    },
+
+    date() {
+        return {
+            title: "",
+            content: ""
+        }
+    },
+
+    methods: {
+        store() {
+            this.$inertia.post('/posts', {title: this.title, content: this.content})
+        }
     }
 }
 </script>
